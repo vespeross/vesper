fbc058a79f4d7d6af853de1ca43390166a07ae36
hosenur <hosenur.dev@gmail.com>
2024-05-19 19:29:17 +0530

    init: layout

---
+++ apps/studio/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { AppLayout } from '@/layouts/AppLayout'
+
+export default function Dashboard() {
+    return (
+        <AppLayout>
+            <div>Dashboard</div>
+        </AppLayout>
+    )
+}
