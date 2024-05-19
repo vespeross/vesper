d6db3c54ef5ebb5304c115a16292ded032b299ed
jabedzaman <hi@jabed.dev>
2024-05-19 20:39:44 +0530

    feat: wrap app with redux provider

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </React.StrictMode>
+);
