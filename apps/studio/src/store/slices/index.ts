e3fc8aeea1dbdd4ec22204d341c12714846cec20
hosenur <hosenur.dev@gmail.com>
2024-05-21 04:45:33 +0530

    refactor: rename user to auth

--- apps/studio/src/store/slices/index.ts
+++ apps/studio/src/store/slices/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import { reducer as userReducer } from "./user";
+import { reducer as authReducer } from "./auth";
 
 export const rootReducer = combineReducers({
-  user: userReducer,
+  auth: authReducer,
 });
