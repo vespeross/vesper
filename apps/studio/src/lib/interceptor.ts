9b06fb26647f5b5e32a5f31a4fceef736a1759b8
hosenur <hosenur.dev@gmail.com>
2024-05-21 05:30:05 +0530

    refactor: move API url to env

--- apps/studio/src/lib/interceptor.ts
+++ apps/studio/src/lib/interceptor.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`
 });
 
 instance.interceptors.request.use(
