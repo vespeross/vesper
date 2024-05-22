4428e36ca0abdaefd76d085c79dac78f47d27b30
jabedzaman <hi@jabed.dev>
2024-05-22 06:20:41 +0530

    feat: add persistant storage

--- apps/api/src/modules/projects/interfaces/project.service.d.ts
+++ apps/api/src/modules/projects/interfaces/project.service.d.ts
@@ -2,11 +2,8 @@ import { CreateProjectDto } from '../dtos';
 import { CreateProjectResponse } from './project.response';
 
 export type IProjectService = {
-  createProject: ({
-    project,
-    userId,
-  }: {
-    project: CreateProjectDto;
-    userId: string;
-  }) => Promise<CreateProjectResponse>;
+  createProject: (
+    project: CreateProjectDto,
+    userId: string,
+  ) => Promise<CreateProjectResponse>;
 };
