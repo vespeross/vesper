578cb644f7b3d3476a63a1326d04bdf60e5079be
hosenur <hosenur.dev@gmail.com>
2024-05-23 03:40:49 +0530

    merge

--- apps/studio/src/pages/auth/index.tsx
+++ apps/studio/src/pages/auth/index.tsx
@@ -4,6 +4,7 @@ import { Register } from './register';
 
 export default function Auth() {
     const { data } = useNewQuery()
+    console.log(data?.body)
     if (!data) return null
     else if (data.body.isNewInstall) return <Register />
     else return <Login />
