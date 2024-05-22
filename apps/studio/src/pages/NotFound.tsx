b8a6f1e90138b7df01d5761211a75700a711f52d
hosenur <hosenur.dev@gmail.com>
2024-05-23 04:26:13 +0530

    fix broken links

--- apps/studio/src/pages/NotFound.tsx
+++ apps/studio/src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className="flex flex-col justify-center text-center h-screen items-center">
       <div className="font-mono">
         <p>WUPS, PAGE NOT FOUND</p>
-        <Link to={isAuthenticated ? "/dashboard" : "/auth/login"}>
+        <Link to={isAuthenticated ? "/dashboard" : "/auth"}>
           GO {isAuthenticated ? "BACK TO DASHBOARD" : "LOGIN"}
         </Link>
       </div>
