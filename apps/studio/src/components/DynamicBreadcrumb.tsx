fc3da09389e2f568129edba6a99c1ee66520f9f1
hosenur <hosenur.dev@gmail.com>
2024-05-28 03:16:31 +0530

    handle dynamic breadcrumb

--- apps/studio/src/components/DynamicBreadcrumb.tsx
+++ apps/studio/src/components/DynamicBreadcrumb.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from './ui/breadcrumb'
+import { useProject } from '@/hooks'
 
 export default function DynamicBreadcrumb() {
     const { pathname } = useLocation()
 
     const pathParts = pathname.split('/').filter(part => part)
+    const isProject = pathParts.includes('projects')
+    const projectIdIndex = pathParts.indexOf('projects') + 1
+    const projectId = pathParts[projectIdIndex]
+    const { project } = useProject(isProject && projectId ? projectId : "Unknown Project")
 
     const capitalize = (str: string) => {
         return str.replace(/\b\w/g, char => char.toUpperCase())
@@ -17,8 +22,8 @@ export default function DynamicBreadcrumb() {
                 {pathParts.map((part, index) => {
                     const isLast = index === pathParts.length - 1
                     const pathTo = '/' + pathParts.slice(0, index + 1).join('/')
-
-                    const modifiedPart = (index > 1 && pathParts[index - 1] === 'projects') ? 'xyz' : part
+                    
+                    const modifiedPart = isProject && index === projectIdIndex ? project?.name || 'xyz' : part
                     const capitalizedPart = capitalize(modifiedPart)
 
                     return (
