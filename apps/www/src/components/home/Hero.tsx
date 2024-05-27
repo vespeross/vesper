868f1fae7ff38f27acb364964e752e8ddce4dc2f
jabedzaman <hi@jabed.dev>
2024-05-28 05:13:42 +0530

    feat: add dashboard screenshot to www

--- apps/www/src/components/home/Hero.tsx
+++ apps/www/src/components/home/Hero.tsx
@@ -1,18 +1,15 @@
 import * as React from "react";
-import { bricolage, sora } from "@/lib/fonts";
-import { Window } from "../common";
 import Image from "next/image";
 
 export const Hero: React.FC = React.memo(() => {
   return (
-    <Window title="app.vesper.rest" isBrowser className="m-2 select-none">
-      {/* <Image src="/dashboard.png" alt="dashboard" className="select-none" width={1600} height={1600} /> */}
-      <div className="h-[60vh] bg-black flex justify-center items-center">
-        <p>
-          Add Screenshot here
-        </p>
-      </div>
-    </Window>
+    <Image
+      src="/dashboard.png"
+      alt="dashboard"
+      className="select-none"
+      width={1600}
+      height={1600}
+    />
   );
 });
 
