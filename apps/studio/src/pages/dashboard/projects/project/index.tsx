3885ea014312a13b6ce1cd9638aacce9dfd2cc0a
jabedzaman <hi@jabed.dev>
2024-05-27 23:38:25 +0530

    feat: add dynamic route for project

--- apps/studio/src/pages/dashboard/projects/project/index.tsx
+++ apps/studio/src/pages/dashboard/projects/project/index.tsx
@@ -1,7 +1,17 @@
+import { useProjects } from "@/hooks";
 import * as React from "react";
 import { useParams } from "react-router-dom";
 
 export const Project: React.FC = () => {
-  const { projectId } = useParams<{ projectId: string }>();
-  return <div>Project {projectId}</div>;
+  const { cid } = useParams<{ cid: string }>();
+  const { isLoading, projects } = useProjects();
+  if (isLoading) return <div>Loading...</div>;
+  const project = projects.find((project) => project.cid === cid);
+  if (!project) return <div>Project not found for {cid}</div>;
+  return (
+    <div>
+      Project
+      <pre>{JSON.stringify(project, null, 4)}</pre>
+    </div>
+  );
 };
