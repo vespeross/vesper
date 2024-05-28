65fdbb18fdc290027a37ae0866d1bad1c1ab1612
jabedzaman <hi@jabed.dev>
2024-05-28 05:36:28 +0530

    feat: improve loading

--- apps/studio/src/layouts/root.layout.tsx
+++ apps/studio/src/layouts/root.layout.tsx
@@ -2,13 +2,18 @@ import * as React from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useGetMeQuery } from "@/store/slices/auth";
 import { useUser } from "@/hooks";
+import { Icons } from "@/components/ui/icons";
 
 export const RootLayout: React.FC = () => {
   const { isLoading } = useGetMeQuery({});
   const location = useLocation();
   const { isAuthenticated } = useUser();
   if (isLoading) {
-    return null;
+    return (
+      <div className="flex justify-center items-center min-h-[calc(100vh-4rem)]">
+        <Icons.spinner className="animate-spin h-5 w-5" />
+      </div>
+    );
   }
   if (!isAuthenticated && location.pathname !== "/auth") {
     return <Navigate to="/auth" />;
--- apps/studio/src/pages/handler.tsx
+++ apps/studio/src/pages/handler.tsx
@@ -2,12 +2,17 @@ import * as React from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "@/hooks";
 import { useGetMeQuery } from "@/store/slices/auth";
+import { Icons } from "@/components/ui/icons";
 
 export const Handler: React.FC = () => {
   const { isLoading } = useGetMeQuery({});
   const { isAuthenticated } = useUser();
   if (isLoading) {
-    return null;
+    return (
+      <div className="flex justify-center items-center min-h-[calc(100vh-4rem)]">
+        <Icons.spinner className="animate-spin h-5 w-5" />
+      </div>
+    );
   }
   return isAuthenticated ? (
     <Navigate to="/dashboard" />
